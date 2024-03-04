1eb6232a729f426e1edc4693ce2edce347734d5b
Jan Sebastian Siwy <github@jansiwy.de>
2024-03-04 09:53:14 +0100

    Disable SES domain verification

--- ses.tf
+++ ses.tf
@@ -1,9 +1,3 @@
 resource "aws_ses_domain_identity" "this" {
   domain = var.domain_name
 }
-
-resource "aws_ses_domain_identity_verification" "this" {
-  domain = aws_ses_domain_identity.this.domain
-
-  depends_on = [aws_route53_record.domainkey_cname]
-}
